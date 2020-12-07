db7dfc924e32ea56112b0c7d6f11070ee765801d
Marah-uwase <maranahuwase12@gmail.com>
2020-12-07 12:39:25 +0200

    configuring the secret key

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = 'SECRET_KEY'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://maranatha:marahuwase@localhost/oneminutepitch'
 
 
