d27e6e9ea73be5e51595c2420a7b41e5c2b34c30
Marah-uwase <maranahuwase12@gmail.com>
2020-12-07 21:34:49 +0200

    export the mail and pssword

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY= 'SECRET_KEY'
-# export MAIL_USERNAME= 'maranahuwase12@gmail.com'
-# export MAIL_PASSWORD= 'marahuwase@31998'
+export MAIL_USERNAME= 'maranahuwase12@gmail.com'
+export MAIL_PASSWORD= 'marahuwase@31998'
 export SQLALCHEMY_DATABASE_URI='postgresql://maranatha:marah31998@localhost/oneminutepitch'
 
 python3.6 manage.py server
